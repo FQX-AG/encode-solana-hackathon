02d6969f4b96b671c561d52648929a2ed72c8cee
Daniel Killenberger <daniel.killenberger@gmail.com>
2024-01-23 10:51:43 +0100

    init pnpm and add pretter and eslint for programs

--- programs/tests/utils.ts
+++ programs/tests/utils.ts
@@ -1,25 +1,25 @@
-import {Connection, PublicKey, Signer} from "@solana/web3.js";
+import { Connection, PublicKey, Signer } from "@solana/web3.js";
 import * as anchor from "@coral-xyz/anchor";
 
 export type PDA = {
-    publicKey: PublicKey;
-    bump: number;
+  publicKey: PublicKey;
+  bump: number;
 };
 export async function newAccountWithLamports(
-    connection: Connection,
-    lamports = 100000000000
+  connection: Connection,
+  lamports = 100000000000
 ): Promise<Signer> {
-    const account = anchor.web3.Keypair.generate();
-    const signature = await connection.requestAirdrop(
-        account.publicKey,
-        lamports
-    );
-    const { blockhash, lastValidBlockHeight } =
-        await connection.getLatestBlockhash({ commitment: "confirmed" });
-    await connection.confirmTransaction({
-        signature,
-        blockhash,
-        lastValidBlockHeight,
-    });
-    return account;
+  const account = anchor.web3.Keypair.generate();
+  const signature = await connection.requestAirdrop(
+    account.publicKey,
+    lamports
+  );
+  const { blockhash, lastValidBlockHeight } =
+    await connection.getLatestBlockhash({ commitment: "confirmed" });
+  await connection.confirmTransaction({
+    signature,
+    blockhash,
+    lastValidBlockHeight,
+  });
+  return account;
 }
