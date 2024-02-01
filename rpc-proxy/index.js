2e1d263f954dd24d5042bc31014de419eca81a88
dawid.stezycki <dawid.stezycki@gmail.com>
2024-02-01 17:40:37 +0100

    fix: body parser in rpc-proxy

--- rpc-proxy/index.js
+++ rpc-proxy/index.js
@@ -9,15 +9,22 @@ const app = express();
 const API_KEY = process.env.IRONFORGE_API_KEY;
 const TARGET_URL = `https://rpc.ironforge.network/devnet?apiKey=${API_KEY}`;
 
+app.use(express.json());
+
 app.use(
   "/",
   createProxyMiddleware({
     target: TARGET_URL,
     changeOrigin: true,
     ws: true,
-    logLevel: "silent",
+    onProxyReq: (proxyReq, req, res) => {
+      console.log("============");
+      console.log(req.body);
+    },
+    logLevel: "debug",
   })
 );
+
 app.listen(3000, () => {
   console.log(`Server is running on port 3000`);
 });
