8a8cc416deba47879b866887ceb65958f2ed36bf
Daniel Killenberger <daniel.killenberger@gmail.com>
2024-02-04 14:04:37 +0100

    make dummy oracle use gaussian for new price

--- backend/src/structured-product/oracle.service.ts
+++ backend/src/structured-product/oracle.service.ts
@@ -26,38 +26,53 @@ export class OracleService {
       this.configService.get<string>('PAYMENT_TOKEN_MINT_ADDRESS'),
     );
   }
+
   @Interval(5000)
   async updatePricing() {
+    // Utility function to generate Gaussian random numbers
+    function gaussianRandom(mean, sigma) {
+      let u = 0,
+        v = 0;
+      while (u === 0) u = Math.random(); // Converting [0,1) to (0,1)
+      while (v === 0) v = Math.random();
+      const num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+      return num * sigma + mean;
+    }
+
     const oldPrice = await this.serverSdk.getCurrentPriceFromDummyOracle(
       'CRZYBTC',
       this.serverSdk.provider.publicKey,
     );
-    const onePercentValue = oldPrice.currentPrice.divn(20).toNumber();
-    const newPriceDelta = new BN(
-      this.randomNegativeOrPositiveInRange(onePercentValue),
-    );
+    const relativeChangeRangeLimit = 0.1;
+    const absoluteChangeRangeLimit = oldPrice.currentPrice
+      .muln(relativeChangeRangeLimit)
+      .toNumber();
+
+    // Generate a Gaussian random number
+    const randomChange = gaussianRandom(0, 1) * absoluteChangeRangeLimit;
+
+    const newPriceDelta = new BN(randomChange);
     const newPrice = oldPrice.currentPrice.add(newPriceDelta);
     const setPriceIx =
       await this.serverSdk.createSetPriceDummyOracleInstruction(
         'CRZYBTC',
         newPrice,
       );
+
     this.logger.log({
       msg: 'Setting new Price',
       newPrice: newPrice.toString(),
+      newPriceDelta: newPriceDelta.toString(),
     });
+
     const updatePriceTxId = await this.serverSdk.sendAndConfirmV0Tx([
       setPriceIx,
     ]);
+
     this.logger.log({
       msg: 'Price updated',
       newPrice: newPrice.toString(),
       txId: updatePriceTxId,
     });
-  }
-
-  randomNegativeOrPositiveInRange(range: number) {
-    const plusOrMinus = Math.round(Math.random()) * 2 - 1;
-    return new BN(randomInt(range) * plusOrMinus);
   }
 }
